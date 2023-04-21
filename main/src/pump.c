55b210a7b801a292931a80aae13a883577497126
Marco Piatti <marcopiatticastro@hotmail.com>
2023-04-21 18:22:49 -0300

    peristencia pump

--- main/src/pump.c
+++ main/src/pump.c
@@ -1,12 +1,18 @@
 #define IS_PUMP_C
 #include <pump.h>
+#include <nvs.h>
 
 pump_t pump;
 
 pump_t pump_init(gpio_num_t pin) {
     pump_t pump;
     pump.pin = pin;
-    pump.state = PUMP_OFF;
+
+    nvs_handle_t nvs_handle;
+    nvs_open("pump", NVS_READWRITE, &nvs_handle);
+    esp_err_t err = nvs_get_u8(nvs_handle, "state", &pump.state);
+    if (err == ESP_ERR_NVS_NOT_FOUND) { pump.state = PUMP_OFF; }
+    nvs_close(nvs_handle);
     
     gpio_config_t pump_config = {
         .pin_bit_mask = 1ULL << pump.pin,
@@ -24,4 +30,10 @@ pump_t pump_init(gpio_num_t pin) {
 void pump_set_state(pump_t *pump, pump_state_t state) {
     pump->state = state;
     gpio_set_level(pump->pin, state==PUMP_OFF ? 0 : 1);
+
+    nvs_handle_t nvs_handle;
+    nvs_open("pump", NVS_READWRITE, &nvs_handle);
+    nvs_set_u8(nvs_handle, "state", pump->state);
+    nvs_commit(nvs_handle);
+    nvs_close(nvs_handle);
 }
